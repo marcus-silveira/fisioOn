a9ec52c0758693d79b927b67b68566d7ad0762f6
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-09-09 22:09:57 -0300

    Finalizado configs de themes e statusBar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,25 @@
+import { ThemeProvider } from "styled-components/native";
+import { StatusBar } from "react-native";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import theme from "./src/theme";
+
 import { Groups } from "@screens/Groups";
+import { Loading } from "@components/Loading";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={"transparent"}
+        translucent
+      ></StatusBar>
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  );
 }
