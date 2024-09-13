384e8a5a19c30c1f9acbd2ddf933d0a4f6d7987a
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-09-12 21:57:53 -0300

    Finalizado telas. Realizar a paginacao agora

---
+++ src/components/PatientsCard/index.tsx
@@ -0,0 +1,20 @@
+import { ButtonIcon } from "@components/ButtonIcon";
+
+import { Container, Icon, Name } from "./style";
+
+type Props = {
+  name: string;
+  onRemove: () => void;
+};
+
+export function PatientsCard({ name, onRemove }: Props) {
+  return (
+    <Container>
+      <Icon name="person" />
+
+      <Name>{name}</Name>
+
+      <ButtonIcon icon="close" type="SECONDARY" onPress={onRemove} />
+    </Container>
+  );
+}
--- src/screens/Patients/index.tsx
+++ src/screens/Patients/index.tsx
@@ -1,20 +1,63 @@
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 
-import { Container, Form } from "./style";
+import { useState } from "react";
+import { FlatList } from "react-native";
+
+import { Container, Form, HeaderList, NumberOfPatients } from "./style";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
+import { PatientsCard } from "@components/PatientsCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Patients() {
+  const [days, setDays] = useState("SEGUNDA");
+  const [Patients, setPatients] = useState(["Enio", "Lucas", "João"]);
+
   return (
     <Container>
       <Header showBackButton />
-      <Highlight title="Nome da Clínica" subtitle="adicione seus pacientes" />
+      <Highlight title="Catamarca" subtitle="adicione seus pacientes" />
       <Form>
-        <Input placeholder="Nome da pessoa" autoCorrect={false} />
-
+        <Input placeholder="Nome do paciente" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          data={["SEGUNDA", "TERÇA", "QUARTA", "QUINTA", "SEXTA"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === days}
+              onPress={() => setDays(item)}
+            />
+          )}
+          horizontal
+        />
+
+        {/* <NumberOfPatients>{Patients.length}</NumberOfPatients> */}
+      </HeaderList>
+      <FlatList
+        data={Patients}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PatientsCard name={item} onRemove={() => {}} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          Patients.length === 0 && { flex: 1 },
+        ]}
+      />
+
+      <Button title="Remover Clínica" type="SECONDARY" />
     </Container>
   );
 }
